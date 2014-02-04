e2e1d87d5d40dd545f795a5d7433b38ddddb2bb1
João Carvalho <joao.pedro.carvalho@tecnico.ulisboa.pt>
2014-02-04 18:10:56 +0000

    Fixed Application's access groups

--- src/main/java/net/sourceforge/fenixedu/presentationTier/Action/person/PersonApplication.java
+++ src/main/java/net/sourceforge/fenixedu/presentationTier/Action/person/PersonApplication.java
@@ -5,19 +5,19 @@ import org.fenixedu.bennu.portal.StrutsApplication;
 public class PersonApplication {
 
     @StrutsApplication(descriptionKey = "label.navheader.person", path = "personal-area", titleKey = "label.navheader.person",
-            bundle = "resources.ApplicationResources")
+            bundle = "resources.ApplicationResources", accessGroup = "role(PERSON)")
     public static class PersonalAreaApp {
 
     }
 
     @StrutsApplication(descriptionKey = "label.homepage", path = "homepage", titleKey = "label.homepage",
-            bundle = "resources.ApplicationResources")
+            bundle = "resources.ApplicationResources", accessGroup = "role(PERSON)")
     public static class HomepageApp {
 
     }
 
     @StrutsApplication(descriptionKey = "oauthapps.label", path = "external-applications", titleKey = "oauthapps.label",
-            bundle = "resources.ApplicationResources")
+            bundle = "resources.ApplicationResources", accessGroup = "role(PERSON)")
     public static class ExternalApplicationsApp {
 
     }
